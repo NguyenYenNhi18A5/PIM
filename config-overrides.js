2e050de973bae13424962839edf0e0e6c6102ebe
YenNhiCode <nguyenyennhi188188@gmail.com>
2022-05-26 02:58:56 +0700

    Cài đặt và cấu hình

--- config-overrides.js
+++ config-overrides.js
@@ -2,8 +2,5 @@ module.exports = function override(config, env) {
     //do stuff with the webpack config...
     return config;
 };
-const {
-    override, useBabelRc } = require("customize-cra");
-module.exports = override(
-    useBabelRc()
-);   
+const { override, useBabelRc } = require('customize-cra');
+module.exports = override(useBabelRc());
